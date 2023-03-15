d63e2d1a2f21a6907f7b33dad7d137c32f826ce5
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2023-03-15 13:16:49 +0300

    fix utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,10 +3,11 @@ import { FriendData } from "../types/types";
 export const handleFriends = (
   friends: Array<{ [k: string]: any }>
 ): FriendData[] => {
-  return friends.map((i) => ({
+  const res: FriendData[] = friends.map((i) => ({
     first_name: i?.first_name ?? "",
     last_name: i?.last_name ?? "",
     photo_100: i?.photo_100 ?? "",
-    id: i?.id ?? "",
+    id: i?.id ?? Math.random(),
   }));
+  return res;
 };
